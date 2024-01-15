57f6bb68acc66255ae8365533020b4d1a3124745
Jiwon Chang <me@jiwonc.net>
2024-01-15 15:38:38 -0500

    removed outdated functions

--- dt/dt.py
+++ dt/dt.py
@@ -7,20 +7,6 @@ import math
 import psycopg2
 from sklearn.preprocessing import LabelEncoder
 
-class CSVChunkReader:
-    def __init__(self, filename, chunksize=1000):
-        self.filename = filename
-        self.chunksize = chunksize
-        self.reader = pd.read_csv(filename, chunksize=chunksize)
-    
-    def next(self):
-        try:
-            chunk = next(self.reader)
-            return chunk
-        except StopIteration:
-            self.reader = None
-            return None
-
 class DBSource:
         def __init__(self, host, database, user, password, query, y_name):
                 self.host = host
@@ -290,63 +276,3 @@ def parse_slices(slices):
             reformatted_slice.append(feature)
         reformatted_slices.append(reformatted_slice)
     return reformatted_slices
-
-# Random CSV files will each have features a, b, c
-# a = 0 or 1
-# b = 0 to 2
-# c = 0 to 3
-# Subgroup stats: 
-#   000   001   002   003   010   011   012   013   020   021   022   023   100   101   102   103   110   111   112   113   120   121   122   123
-# -----------------------------------------------------------------------------------------------------------------------------------------------
-# 0 2634  3970  5882  0     873   1308  1966   0,   3419  5230  7893  0     5283  7805  11878 0     1798  2623  4013  0     7001  10351 16073 0
-# 1 137   2211  147   255   739   11082 729    1429 901   13247 870   1763  315   4438  289   587   1412  21901 1461  2873  1769  26300 1694  3451
-# 2 3291  2230  964   960   3687  2531  1122   1020 456   288   125   151   15602 11134 4830  4671  18163 12840 5348  5430  2281  1498  696   682
-# 3 4545  4545  4497  872   1122  1154  1164   241  3398  3304  3356  630   11114 11119 11229 2273  2801  2800  2815  549   8154  8193  8428  1697
-# 4 1281  604   856   366   7569  3705  5316   2323 6347  3032  4267  1868  2102  1062  1499  633   12539 6151  8824  3698  10479 5249  7159  3071
-
-if __name__ == '__main__':
-
-        sources = [
-                'data/random_csv_0.csv',
-                'data/random_csv_1.csv',
-                'data/random_csv_2.csv',
-                'data/random_csv_3.csv',
-                'data/random_csv_4.csv'
-        ]
-
-        costs = np.array([1.0, 1.0, 1.0, 1.0, 1.0])
-
-        # Slices used for random CSV test case:
-        # 11X
-        # 011
-        # 111
-        slices = [
-            ['(0.5,inf)', '(0.5,1.5)', '(-inf,inf)'],
-            ['(-inf,0.5)', '(0.5,1.5)', '(0.5,1.5)'],
-            ['(0.5,inf)', '(0.5,1.5)', '(0.5,1.5)']
-        ]
-
-        #   11X   023   111
-        # 0 8434  0     2623
-        # 1 27647 1763  21901
-        # 2 41781 151   12840
-        # 3 8965  630   2800
-        # 4 31212 1868  6151
-        stats = [
-            [8434, 1308, 2623],
-            [27647, 11082, 21901],
-            [41781, 2531, 12840],
-            [8965, 1154, 2800],
-            [31212, 3705, 6151],
-        ]
-        
-        dt = DT(sources, costs, slices, None, batch=100)
-        print(dt)
-        
-        query_counts = [
-            1000,
-            1000,
-            1000
-        ]
-
-        dt.run(query_counts)
--- dt/pipeline.py
+++ dt/pipeline.py
@@ -2,7 +2,7 @@ import numpy as np
 import numpy.typing as npt
 import pandas as pd
 from typing import Tuple
-from .dt import *
+from dt import *
 
 """
 Defines and exposes the API functions to client programs. 
@@ -102,4 +102,98 @@ def pipeline_dt_py(
             selected_slices (npt.NDArray): A sequence of slice indices (based
                 on the input)
     """
+    return dt_results
+
+def pipeline_train_dml(
+    train_x: pd.DataFrame,
+    train_y: npt.NDArray[np.float64],
+    test_x: pd.DataFrame,
+    test_y: npt.NDArray[np.float64],
+    max_depth: int
+) -> Tuple[pd.DataFrame, np.ndarray, dict]:
+    """
+    params:
+        train_x, train_y: Training data in "train" format.
+        test_x, test_y: Test set in "train" format.
+        max_depth: The maximum depth of slices to be considered for the slice_losses
+                   return value.
+    returns:
+        train_x: Train set in "train" format. 
+        train_losses: 1D array, train set losses. 
+        train_stats: A dictionary to store various statistics about training
+            session. It should at least contain the following key-value pairs:
+            time_train (float): Time, in seconds, that training took. 
+            time_func (float): Time, in seconds, that this entire function took. 
+            agg_loss (float): Average loss (or accuracy) across all data points. 
+            slice_losses (pd.DataFrame): A dataframe containing all slices up to
+                max_depth in "DT" format, and additional "loss" column which
+                stores average loss (or accuracy) for that slice, and "size"
+                column which stores the size of that slice in train set. 
+    """
+    # TODO: DML Implementation & call Python binding
+    return train_x_binned, train_losses, train_stats
+
+def pipeline_sliceline_dml(
+    train_x: pd.DataFrame,
+    train_losses: npt.NDArray[np.float64],
+    alpha: float,
+    max_l: int,
+    min_sup: int,
+    k: int
+) -> Tuple[pd.DataFrame, dict]:
+    """
+    params:
+        train_x: Train set in "train" format. 
+        train_losses: Train losses as 1D array. 
+        alpha, max_l, min_sup, k: Standard sliceline parameters. 
+    returns:
+        slices: Top slices in "DT" format. It should also contain the additional
+            columns "loss", "size", and "score". 
+        sliceline_stats: A dictionary to store various statistics about the
+            run of sliceline. It should contain the following key-value pairs:
+            time_sliceline (float): Time, in seconds, that sliceline took. 
+            time_func (float): Time, in seconds, that this entire function took. 
+    """
+    # TODO: DML Implementation & call Python binding
+    return slices, sliceline_stats
+
+def pipeline_dt_dml(
+    sources: List[dt.DBSource],
+    groups: pd.DataFrame,
+    costs: npt.NDArray,
+    query_counts: npt.NDArray,
+    undersample_method: str,
+    train_x: pd.DataFrame,
+    train_y: np.ndarray,
+    algos: List[str],
+) -> dict:
+    """
+    params:
+        sources: A list of DBSource objects that contain data sources metadata. 
+        groups: A dataframe in "DT" format of the top slices. 
+        costs: Cost model for each source. 
+        query_counts: Query count for each slice. 
+        undersample_method: Either "random", "medoids", or "none". Used to 
+            undersample majority groups' excess data points. 
+        train_x, train_y: Current train set in "train" format. 
+        algos: A list of algorithms to run one after another. Valid values are:
+            "random", "ratiocoll", "exploreexploit". 
+    returns:
+        A dictionary that maps from algorithm name to its results. 
+        A valid key is a valid algorithm name, as explained above. 
+        A valid value is a tuple (aug_x, aug_y, dt_stats): (pd.DataFrame, 
+        pd.DataFrame, dict). aug_x and aug_y are train sets augmented with the
+        additional data found. It MUST be shuffled. 
+        dt_stats is a dictionary to store various statistics about the run of
+        the DT algorithm specified in the key. It should contain the following
+        key-value pairs:
+            time (float): Time, in seconds, that DT took. 
+            iters (int): The number of iterations that was required. 
+            cost (float): The total cost required to satisfy query. 
+            selected_sources (npt.NDArray): A sequence of sources that the DT
+                algorithm chose. 
+            selected_slices (npt.NDArray): A sequence of slice indices (based
+                on the input)
+    """
+    # TODO: DML Implementation & call Python binding
     return dt_results
